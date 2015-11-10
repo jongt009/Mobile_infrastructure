e408853aea9c2d021b0940cb4b9d491effdb5db0
Thom <thom.de.jong2@hva.nl>
2015-11-10 16:52:52 +0100

    Added QR scanner functionality

--- QR_Reader_Application/app/src/main/java/com/example/thomdejong/qr_reader_application/MainActivity.java
+++ QR_Reader_Application/app/src/main/java/com/example/thomdejong/qr_reader_application/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.thomdejong.qr_reader_application;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,10 +33,53 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                openQRScanner();
             }
         });
+    }
+
+    // opens the QR scanner
+    private void openQRScanner(){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        // only search for QR codes
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        integrator.setPrompt("Scan a QR code");
+        // Start scan
+        integrator.initiateScan();
+    }
+
+    // Get result of the QR scanner
+    // Called when QR code is found on camera
+    @Override
+    protected void onActivityResult(int requestcode, int resultcode, Intent intent){
+        // Get result
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestcode, resultcode, intent);
+        String contents = intentResult.getContents();
+
+        if(isUrl(contents)) {
+            // Open URL
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse(contents));
+            startActivity(browserIntent);
+        }else {
+            // Set text
+            TextView textView = (TextView) findViewById(R.id.text);
+            textView.setText(contents);
+        }
+    }
+
+    private boolean isUrl(String value){
+        boolean result = true;
+        try {
+            // Checks start with "http(s)://"
+            URL url = new URL(value);
+            // Checks buildup of url "text.text"
+            url.toURI();
+        } catch (MalformedURLException e){
+            result = false;
+        } catch (URISyntaxException e) {
+            result = false;
+        }
+        return result;
     }
 
     @Override
